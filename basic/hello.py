737feb60d4cdf908ed1436949621e05194013c8c
Sean Chen <sean.chen@leocorn.com>
2021-03-07 00:55:10 +0000

    update with first challenge!

--- basic/hello.py
+++ basic/hello.py
@@ -1,3 +1,4 @@
-print("hello Python!")
+print("hello welcome Python!")
 name = input("What is your name?")
-print("Hello, " + name)
+print("Hello, " + name + " Nice to meet you!")
+input("Press Enter to exit!")
